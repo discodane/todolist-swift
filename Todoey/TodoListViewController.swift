57c5ce3889b7e40ffb60a52b308f1960ba6af1fc
Dane Erickson <Dane.Erickson@jbhunt.com>
2020-04-21 23:29:35 -0400

    Adds data persistence

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,21 +11,30 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        print("something")
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+            self.tableView.reloadData()
         }
         
         alert.addTextField { (alertTextField) in
@@ -35,7 +44,7 @@ class TodoListViewController: UITableViewController {
         
         alert.addAction(action)
         
-        self.tableView.reloadData()
+        present(alert, animated: true, completion: nil)
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
